31522be2d9991ac46aad14c83ae7ce78bb429108
T-Hash06 <tomasfelipe.p@gmail.com>
2024-11-23 11:35:46 -0500

    docs: add swagger API docs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,12 +4,27 @@ import { Token } from '@auth/auth.decorators';
 import { LoginDto, LoginValidationPipe } from '@auth/auth.dto';
 import { AuthService } from '@auth/auth.service';
 import { Body, Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+	ApiBearerAuth,
+	ApiHeader,
+	ApiOperation,
+	ApiResponse,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
 @Controller('auth')
 export class AuthController {
 	constructor(private authService: AuthService) {}
 
 	@Get('session')
+	@ApiOperation({ summary: 'Get the current session' })
+	@ApiHeader({
+		name: 'authorization',
+		description: 'Bearer <Token>',
+		required: true,
+	})
+	@ApiResponse({ status: 200, description: 'The current session' })
+	@ApiResponse({ status: 401, description: 'Unauthorized' })
 	getSession(@Token() token: string): ControllerResponse<Session> {
 		const session = this.authService.decodeSession(token);
 
@@ -17,6 +32,11 @@ export class AuthController {
 	}
 
 	@Post('login')
+	@ApiOperation({ summary: 'Log in a user' })
+	@ApiResponse({ status: 201, description: 'The JWT token' })
+	@ApiResponse({ status: 400, description: 'Bad request' })
+	@ApiResponse({ status: 404, description: 'User not found' })
+	@ApiResponse({ status: 401, description: 'Invalid password' })
 	async loginUser(
 		@Body(LoginValidationPipe) credentials: LoginDto,
 	): Promise<ControllerResponse<string>> {
@@ -26,6 +46,14 @@ export class AuthController {
 	}
 
 	@Patch('session')
+	@ApiOperation({ summary: 'Refresh the current session' })
+	@ApiHeader({
+		name: 'Authorization',
+		description: 'Bearer <Token>',
+		required: true,
+	})
+	@ApiResponse({ status: 200, description: 'The new JWT token' })
+	@ApiResponse({ status: 401, description: 'Unauthorized' })
 	refreshToken(@Token() token: string): ControllerResponse<string> {
 		const newToken = this.authService.refreshToken(token);
 
@@ -33,6 +61,14 @@ export class AuthController {
 	}
 
 	@Delete('session')
+	@ApiOperation({ summary: 'Log out a user' })
+	@ApiHeader({
+		name: 'Authorization',
+		description: 'Bearer <Token>',
+		required: true,
+	})
+	@ApiResponse({ status: 200, description: 'User logged out successfully' })
+	@ApiResponse({ status: 401, description: 'Unauthorized' })
 	logoutUser(@Token() token: string): ControllerResponse<null> {
 		this.authService.logoutUser(token);
 
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,27 @@ config({ override: false });
 
 import { AppModule } from '@app/app.module';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Auth service')
+		.setDescription('An auth service for authentication and authorization')
+		.setVersion('1.0')
+		.addTag('Auth')
+		.addBearerAuth({
+			type: 'http',
+			scheme: 'bearer',
+			name: 'Authorization',
+			bearerFormat: 'Bearer <token>',
+		})
+		.build();
+	const documentFactory = (): OpenAPIObject =>
+		SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 
